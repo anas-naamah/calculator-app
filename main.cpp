87327989611e55513f00bd78784fbcdb9cb3270d
Anas Naamah <anasnaamah@gmail.com>
2026-08-31 11:22:49 +0300

    Add subtract function

--- main.cpp
+++ main.cpp
@@ -5,10 +5,16 @@ int add(int a, int b)
     return a + b;
 }
 
+int subtract(int a, int b)
+{
+    return a - b;
+}
+
 int main()
 {
     std::cout << "Calculator App" << std::endl;
     std::cout << "2 + 3 = " << add(2, 3) << std::endl;
+    std::cout << "5 - 2 = " << subtract(5, 2) << std::endl;
 
     return 0;
 }
