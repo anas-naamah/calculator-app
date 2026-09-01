0e16b963f665ff69b767256a76e8998984c3c312
Anas Naamah <anasnaamah@gmail.com>
2026-09-01 07:59:19 +0300

    Add New Line And a File Named .gitignore

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
 {
     std::cout << "Calculator App" << std::endl;
     std::cout << "2 + 3 = " << add(2, 3) << std::endl;
+    std::cout << "5 + 7 = " << add(5, 7) << std::endl;
 
     return 0;
 }
